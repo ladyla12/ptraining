e5ccb77b4c235af23c3789433e91a8130ce082c3
ladyla12 <tohka.yatogami.vip@gmail.com>
2017-01-23 00:01:38 +0700

    Assets folder

---
+++ assets/js/tinymce-init.js
@@ -0,0 +1,36 @@
+tinymce.init({
+  selector: 'textarea',
+  height: 500,
+  theme: 'modern',
+  plugins: [
+    'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+    'searchreplace wordcount visualblocks visualchars code fullscreen',
+    'insertdatetime media nonbreaking save table contextmenu directionality',
+    'emoticons template paste textcolor colorpicker textpattern imagetools codesample'
+  ],
+  toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+  toolbar2: 'print preview media | forecolor backcolor emoticons | fontselect fontsizeselect | codesample',
+  image_advtab: true,
+  templates: [
+    { title: 'Test template 1', content: 'Test 1' },
+    { title: 'Test template 2', content: 'Test 2' }
+  ],
+  content_css: [
+    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+    '//www.tinymce.com/css/codepen.min.css'
+  ]
+  
+  file_browser_callback: function(field, url, type, win) {
+        tinyMCE.activeEditor.windowManager.open({
+            file: 'assets/kcfinder/browse.php?opener=tinymce4&field=' + field + '&type=' + type,
+            title: 'KCFinder',
+            width: 700,
+            height: 500,
+            inline: true,
+            close_previous: false
+        }, {
+            window: win,
+            input: field
+        });
+        return false;
+ });
